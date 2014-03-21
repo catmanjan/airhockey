56a5654c4d6a38dcb6638713736f303efac2df76
Jan Martin <jan.ramon.martin@gmail.com>
2014-03-21 12:42:20 +1100

    Added more correct collision detection

--- airhockey/src/au/com/twosquared/airhockey/PlayState.java
+++ airhockey/src/au/com/twosquared/airhockey/PlayState.java
@@ -2,7 +2,6 @@ package au.com.twosquared.airhockey;
 
 import org.flixel.FlxG;
 import org.flixel.FlxPoint;
-import org.flixel.FlxSprite;
 import org.flxbox2d.B2FlxB;
 import org.flxbox2d.B2FlxState;
 import org.flxbox2d.collision.shapes.B2FlxBox;
@@ -13,16 +12,27 @@ import org.flxbox2d.dynamics.joints.B2FlxMouseJoint;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
 public class PlayState extends B2FlxState {
-	FlxSprite board;
-
+	B2FlxPolygon board;
 	B2FlxCircle puck;
 	B2FlxCircle redHandle;
 	B2FlxCircle blueHandle;
+	B2FlxBox redGoal;
+	B2FlxBox blueGoal;
 
-	float puckDiameter = 220;
-	float handleDiameter = 280;
+	private final float puckSize = 220;
+	private final float handleSize = 280;
 
-	float[][][] gutterVertices = {
+	private final short BOARD = 0x0001;
+	private final short GOAL = 0x0002;
+	private final short PUCK = 0x0004;
+	private final short HANDLE = 0x0008;
+
+	private final short BOARD_MASK = (short) (HANDLE | BOARD | GOAL | PUCK);
+	private final short GOAL_MASK = HANDLE;
+	private final short PUCK_MASK = (short) (HANDLE | BOARD);
+	private final short HANDLE_MASK = (short) (HANDLE | BOARD | GOAL | PUCK);
+
+	float[][][] boardVertices = {
 			{ { 0, 0 }, { 0, 503 }, { 54, 503 }, { 54, 190 } },
 			{ { 0, 0 }, { 54, 190 }, { 134, 110 } },
 			{ { 0, 0 }, { 134, 110 }, { 1914, 110 }, { 2048, 0 } },
@@ -42,38 +52,42 @@ public class PlayState extends B2FlxState {
 		// FlxG.visualDebug = true;
 		// B2FlxDebug.drawBodies = true;
 
-		float puckX = (FlxG.width - puckDiameter) / 2;
-		float puckY = (FlxG.height - puckDiameter) / 2;
-		float blueHandleX = FlxG.width - handleDiameter;
-		float handleY = (FlxG.height - handleDiameter) / 2;
+		float puckX = (FlxG.width - puckSize) / 2;
+		float puckY = (FlxG.height - puckSize) / 2;
+		float blueX = FlxG.width - handleSize;
+		float handleY = (FlxG.height - handleSize) / 2;
 
-		B2FlxPolygon gutter = new B2FlxPolygon(0, 0, gutterVertices);
-		gutter.loadGraphic("pack:board");
-		gutter.offset = new FlxPoint(-2048 / 2, -1536 / 2);
-		gutter.setType(BodyType.StaticBody);
-		gutter.create();
-		add(gutter);
+		board = new B2FlxPolygon(0, 0, boardVertices);
+		board.setCategoryBits(BOARD);
+		board.setMaskBits(BOARD_MASK);
+		board.loadGraphic("pack:board");
+		board.offset = new FlxPoint(-2048 / 2, -1536 / 2);
+		board.setType(BodyType.StaticBody);
+		board.create();
+		add(board);
 
-		box(-1, 0, 1, FlxG.height);
-		box(FlxG.width, 0, 1, FlxG.height);
-
-		puck = circle(puckX, puckY, puckDiameter / 2);
+		puck = circle(puckX, puckY, puckSize / 2, PUCK, PUCK_MASK);
 		puck.loadGraphic("pack:puck");
 
-		blueHandle = circle(blueHandleX, handleY, handleDiameter / 2);
+		blueHandle = circle(blueX, handleY, handleSize / 2, HANDLE, HANDLE_MASK);
 		blueHandle.loadGraphic("pack:blueHandle");
-
-		redHandle = circle(0, handleY, handleDiameter / 2);
+		redHandle = circle(0, handleY, handleSize / 2, HANDLE, HANDLE_MASK);
 		redHandle.loadGraphic("pack:redHandle");
+
+		blueGoal = box(-1, 0, 1, FlxG.height, GOAL, GOAL_MASK);
+		redGoal = box(FlxG.width, 0, 1, FlxG.height, GOAL, GOAL_MASK);
 
 		add(new B2FlxMouseJoint());
 	}
 
-	private B2FlxCircle circle(float x, float y, float radius) {
+	private B2FlxCircle circle(float x, float y, float radius,
+			short categoryBits, short maskBits) {
 		B2FlxCircle circle = new B2FlxCircle(x, y, radius);
+		circle.setCategoryBits(categoryBits);
+		circle.setMaskBits(maskBits);
 		circle.setDensity(1);
-		circle.setFriction(0.1f);
-		circle.setRestitution(0.9f);
+		circle.setFriction(0);
+		circle.setRestitution(0.8f);
 		circle.setFixedRotation(true);
 		circle.setDraggable(true);
 		circle.create();
@@ -82,8 +96,11 @@ public class PlayState extends B2FlxState {
 		return circle;
 	}
 
-	private B2FlxBox box(float x, float y, float width, float height) {
+	private B2FlxBox box(float x, float y, float width, float height,
+			short categoryBits, short maskBits) {
 		B2FlxBox box = new B2FlxBox(x, y, width, height);
+		box.setCategoryBits(categoryBits);
+		box.setMaskBits(maskBits);
 		box.setType(BodyType.StaticBody);
 		box.create();
 		box.visible = false;
