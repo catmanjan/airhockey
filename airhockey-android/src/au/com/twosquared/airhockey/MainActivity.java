02754aa1ccfcdff64382e00eae76b3dcdbe96fb7
Jan <jan.ramon.martin@gmail.com>
2014-08-07 13:45:24 +1000

    version changes

--- airhockey-android/src/au/com/twosquared/airhockey/MainActivity.java
+++ airhockey-android/src/au/com/twosquared/airhockey/MainActivity.java
@@ -38,8 +38,7 @@ public class MainActivity extends FlxAndroidApplication {
 							| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 							| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 							| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-							| View.SYSTEM_UI_FLAG_FULLSCREEN
-							| View.SYSTEM_UI_FLAG_IMMERSIVE);
+							| View.SYSTEM_UI_FLAG_FULLSCREEN);
 		}
 	}
 }
